289a097d27b311b8f7b8d76f0982375a7f7a2edc
liamc9 <liam12crowley@gmail.com>
2025-03-22 17:28:40 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import { NotificationProvider } from "./context/NotificationContext";
+import { AuthProvider, useAuth } from "./context/AuthContext"; // Import useAuth
+import Root from "./routes/Root";
+import Login from "./routes/Login";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import SettingsPage from "./routes/Settings";
+import ManageAccount from "./routes/ManageAccount";
+import Profile from "./routes/Profile";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Helper component for route protection
+const RequireAuth = ({ children }) => {
+  const { currentUser } = useAuth();
+  const location = useLocation();
+  if (!currentUser) {
+    // Redirect to login if not authenticated
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+};
+
+// ROUTES
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
+      { path: "login", element: <Login /> },
+      { path: "home", element: <Home /> },
+      { path: "search", element: <Search /> },
+      {
+        path: "settings/:userId",
+        element: (
+          <RequireAuth>
+            <SettingsPage />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "settings/manageaccount/:userId",
+        element: (
+          <RequireAuth>
+            <ManageAccount />
+          </RequireAuth>
+        ),
+      },
+      { path: "profile/:userId", element: <Profile /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastContainer />
+    <AuthProvider>
+      <NotificationProvider>
+        <RouterProvider router={router} />
+      </NotificationProvider>
+    </AuthProvider>
+    <ToastContainer />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
